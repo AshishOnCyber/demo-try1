94578c06d62d9dc3659f3861d2485ffabb1887a9
AshishOnCyber <ashish.contact.me01@gmail.com>
2024-03-22 14:10:19 +0530

    first commit

---
+++ src/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function nav() {
+  return (
+    <div>
+        <h1>Hello Mats</h1>
+        <p>vcygwdgcuguwdgc dwucguwdg</p>
+    </div>
+  )
+}
+
+export default Nav
